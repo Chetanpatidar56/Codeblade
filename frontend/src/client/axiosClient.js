48eeffe9baae9bac5be252634e2938b8abc14686
Chetan Patidar <cpatidar568@gmail.com>
2025-10-20 17:41:21 +0530

    added cache-control header to axios requests

--- frontend/src/client/axiosClient.js
+++ frontend/src/client/axiosClient.js
@@ -1,9 +1,36 @@
+// import axios from "axios";
+// const axiosClient=axios.create({
+//     baseURL:'https://codeblade.onrender.com',
+//     withCredentials:true,
+//     headers:{
+//         'Content-Type':'application/json'
+//     }
+// });
+// export default axiosClient;
 import axios from "axios";
-const axiosClient=axios.create({
-    baseURL:'https://codeblade.onrender.com',
-    withCredentials:true,
-    headers:{
-        'Content-Type':'application/json'
-    }
+
+const axiosClient = axios.create({
+  baseURL: 'https://codeblade.onrender.com',
+  withCredentials: true,
+  headers: {
+    'Content-Type': 'application/json',
+    'Cache-Control': 'no-cache, no-store, must-revalidate',
+    'Pragma': 'no-cache',
+    'Expires': '0'
+  }
 });
+
+// Add interceptor to ensure cache headers on every request
+axiosClient.interceptors.request.use(
+  (config) => {
+    config.headers['Cache-Control'] = 'no-cache, no-store, must-revalidate';
+    config.headers['Pragma'] = 'no-cache';
+    config.headers['Expires'] = '0';
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
 export default axiosClient;
