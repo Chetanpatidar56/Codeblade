071d489aa4dc0e73a4325549fccfeec54d5692ee
Chetan Patidar <cpatidar568@gmail.com>
2025-10-21 08:32:37 +0530

    update cors origin url

--- backend/src/server.js
+++ backend/src/server.js
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use(cookieParser());
 //allowing frontend port no. to make changes over the database 
 app.use(cors({
-    origin:'https://codeblade-h1j8oupg9-chetan-patidars-projects.vercel.app',
+    origin:'https://codeblade-majtzw6yw-chetan-patidars-projects.vercel.app',
     credentials:true,
      methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
       allowedHeaders: ['Content-Type', 'Cache-Control', 'Pragma', 'Expires']
