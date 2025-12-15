0f107782b4bf3bf39fb526e5beae9896921cccb5
Chetan Patidar <cpatidar568@gmail.com>
2025-12-15 12:50:33 +0530

    Added FAQ

--- frontend/src/components/FAQ.jsx
+++ frontend/src/components/FAQ.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import { Code2, Moon, Sun, Search, ChevronDown, ThumbsUp, ThumbsDown } from 'lucide-react';
-import axios from '../api/axiosClient';
+
 
 const FAQ = () => {
   const { isAuthenticated } = useSelector((state) => state.auth);
